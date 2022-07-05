f7e7bfb98147b10f113d472267e061cd677a845b
Diana Deldar Abdolmaleki <ge54nac@mytum.de>
2022-07-05 10:24:42 +0200

    changed text

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,7 +5,7 @@ public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was a " +
-                    "distinguished English poet, playwright, and actor, widely regarded as the greatest " +
+                    "very distinguished English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         return "";
